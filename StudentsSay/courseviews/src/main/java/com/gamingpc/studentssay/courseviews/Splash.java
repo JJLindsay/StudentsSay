6233cc4c0f08a1e5f8b2d9b00d830a02e48e9d0f
JJLindsay <jlindsay1@ggc.edu>
2015-04-05 16:40:40 -0400

    latest addition has first start on creating the layouts and activities

---
+++ StudentsSay/courseviews/src/main/java/com/gamingpc/studentssay/courseviews/Splash.java
@@ -0,0 +1,24 @@
+package com.gamingpc.studentssay.courseviews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * author: JJ Lindsay
+ * version: 1.0
+ * Course: ITEC 4550 Spring 2015
+ * Written: 4/2/2015
+ *
+ * This class represents a ...
+ *
+ * Purpose: Allows the manipulation of a ...
+ */
+public class Splash extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
